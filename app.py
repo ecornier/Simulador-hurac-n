bb11835f0a09195ab7e38033e9c3404b52b30f01
ecornier <ecornier@albizu.edu>
2026-09-19 09:40:28 -0400

    Fix indentation for data fetching in app.py

--- app.py
+++ app.py
@@ -43,9 +43,10 @@ if modo == "Ciclón tropical histórico":
         ]
     )
     if modo == "Ciclón tropical histórico":
-    datos_hurdat = descargar_hurdat()
-    datos_ciclon = buscar_ciclon(datos_hurdat, ciclón)
-    st.sidebar.write(datos_ciclon)
+        datos_hurdat = descargar_hurdat()
+        datos_ciclon = buscar_ciclon(datos_hurdat, ciclón)
+        st.sidebar.write(datos_ciclon)
+    
 lat = lat = st.sidebar.number_input(
     "Latitud Inicial (°N)",
     min_value=10.0,
