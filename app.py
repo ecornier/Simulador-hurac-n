3966e2c3c89544d6314bd8d42a61b60c4fa4cb19
ecornier <ecornier@albizu.edu>
2026-09-19 10:16:36 -0400

    Fix cyclone search by splitting the input string

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if modo == "Ciclón tropical histórico":
 
 if modo == "Ciclón tropical histórico":
     datos_hurdat = descargar_hurdat()
-    datos_ciclon = buscar_ciclon(datos_hurdat, ciclón)
+    datos_ciclon = buscar_ciclon(datos_hurdat, ciclón.split(" (")[0])
 
     posiciones = datos_ciclon[1:]
 
